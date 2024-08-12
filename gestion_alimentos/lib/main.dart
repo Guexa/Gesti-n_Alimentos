a433f811eeb58ff56597697e6cdf757177625283
Alexa Guerrero López <99861188+Guexa@users.noreply.github.com>
2024-08-11 22:02:44 -0600

    Adding ViewModel to tree

--- gestion_alimentos/lib/main.dart
+++ gestion_alimentos/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gestion_alimentos/viewmodels/caja_viewmodel.dart';
+import 'package:gestion_alimentos/viewmodels/cocina_viewmodel.dart';
+import 'package:gestion_alimentos/viewmodels/corredores_viewmodel.dart';
 import 'package:gestion_alimentos/viewmodels/mesa_viewmodel.dart';
 import 'package:gestion_alimentos/viewmodels/mesero_viewmodel.dart';
+import 'package:gestion_alimentos/viewmodels/perfil_viewmodel.dart';
+import 'package:gestion_alimentos/viewmodels/reporte_viewmodel.dart';
 import 'package:gestion_alimentos/views/login_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -26,7 +31,12 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => EmpleadoViewModel()),
         ChangeNotifierProvider(create: (_) => MesaViewModel()),
-        ChangeNotifierProvider(create: (_) => MeseroViewModel())
+        ChangeNotifierProvider(create: (_) => MeseroViewModel()),
+        ChangeNotifierProvider(create: (_) => CajaViewModel()),
+        ChangeNotifierProvider(create: (_) => CocinaViewModel()),
+        ChangeNotifierProvider(create: (_) => CorredoresViewModel()),
+        ChangeNotifierProvider(create: (_) => PerfilViewModel()),
+        ChangeNotifierProvider(create: (_) => ReporteViewModel())
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
